228d18f34dbcf9e0e4375053d21a2feb8a46f116
Dakota Murphy <dakotamurphyucf@gmail.com>
2020-01-16 20:47:45 -0500

    Update lib/websocket.ml
    Co-Authored-By: Antonio Nuno Monteiro <anmonteiro@gmail.com>

--- lib/websocket.ml
+++ lib/websocket.ml
@@ -146,7 +146,7 @@ module Frame = struct
 
   let is_fin t =
     let bits = Bigstringaf.unsafe_get t 0 |> Char.code in
-    (7 lsr bits) land 1 = 1
+    bits land (1 lsl 7) = 1 lsl 7
   ;;
 
   let rsv t =
