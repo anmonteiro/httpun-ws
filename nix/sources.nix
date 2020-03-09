1e734c11e450d728e14f7b7c7387e4c88dd05aad
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2020-03-08 20:29:06 -0700

    Build on OCaml 4.10 (#9)

--- nix/sources.nix
+++ nix/sources.nix
@@ -1,14 +1,14 @@
-# nix-build -E \
-#  'with import <nixpkgs> { overlays = [(import ./.)];}; pkgs.bs-platform'
-{ ocamlVersion ? "4_09" }:
+{ ocamlVersion ? "4_10" }:
 
 let
   overlays = builtins.fetchTarball {
-    url = https://github.com/anmonteiro/nix-overlays/archive/98b6ee7.tar.gz;
-    sha256 = "12vqdghlz2v7ln28jmrnxy210zv5x21g8kw2s0dvszfmz0p2inkh";
+    url = https://github.com/anmonteiro/nix-overlays/archive/94de6452.tar.gz;
+    sha256 = "1hlwlclhrm60zz90mv55mlacbfd4hddrf04sk6yl6yr9k2clx9sl";
   };
 
-  pkgs = import <nixpkgs> {
+in
+
+  import "${overlays}/sources.nix" {
     overlays = [
       (import overlays)
       (self: super: {
@@ -16,7 +16,4 @@ let
             (super.callPackage "${overlays}/ocaml" {});
       })
     ];
-  };
-
-in
-  pkgs
+  }
