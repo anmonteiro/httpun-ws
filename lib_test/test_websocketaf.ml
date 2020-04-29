874ed2882bf1bcd769bf94dd6eab12eac0c6ca8c
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2020-04-28 21:21:51 -0700

    Fix parsing FIN bit (#11)

--- lib_test/test_websocketaf.ml
+++ lib_test/test_websocketaf.ml
@@ -10,6 +10,17 @@ module Websocket = struct
     | Ok frame -> frame
     | Error err -> Alcotest.fail err
 
+  let serialize_frame ~is_fin frame =
+    let f = Faraday.create 0x100 in
+    Frame.serialize_bytes
+      f
+      ~is_fin
+      ~opcode:`Text
+      ~payload:(Bytes.of_string frame)
+      ~off:0
+      ~len:(String.length frame);
+    Faraday.serialize_to_string f
+
   let test_parsing_ping_frame () =
     let frame = parse_frame "\137\128\000\000\046\216" in
     Alcotest.check Testable.opcode "opcode" `Ping (Frame.opcode frame);
@@ -22,7 +33,8 @@ module Websocket = struct
     let frame = parse_frame "\136\000" in
     Alcotest.check Testable.opcode "opcode" `Connection_close (Frame.opcode frame);
     Alcotest.(check int) "payload_length" (Frame.payload_length frame) 0;
-    Alcotest.(check int) "length" (Frame.length frame) 2
+    Alcotest.(check int) "length" (Frame.length frame) 2;
+    Alcotest.(check bool) "is_fin" true (Frame.is_fin frame)
 
   let test_parsing_text_frame () =
     let frame = parse_frame "\129\139\086\057\046\216\103\011\029\236\099\015\025\224\111\009\036" in
@@ -32,13 +44,25 @@ module Websocket = struct
     Alcotest.(check int) "payload_length" (Frame.payload_length frame) 11;
     Alcotest.(check int) "length" (Frame.length frame) 17;
     Frame.unmask_inplace frame;
+    Alcotest.(check bool) "is_fin" true (Frame.is_fin frame);
     let payload = Bytes.to_string (Frame.copy_payload_bytes frame) in
     Alcotest.(check string) "payload" "1234567890\n" payload
+
+  let test_parsing_fin_bit () =
+   let frame = parse_frame (serialize_frame ~is_fin:false "hello") in
+    Alcotest.check Testable.opcode "opcode" `Text (Frame.opcode frame);
+    Alcotest.(check bool) "is_fin" false (Frame.is_fin frame);
+   let frame = parse_frame (serialize_frame ~is_fin:true "hello") in
+    Alcotest.check Testable.opcode "opcode" `Text (Frame.opcode frame);
+    Alcotest.(check bool) "is_fin" true (Frame.is_fin frame);
+    let payload = Bytes.to_string (Frame.copy_payload_bytes frame) in
+    Alcotest.(check string) "payload" "hello" payload
 
   let tests =
     [ "parsing ping frame",  `Quick, test_parsing_ping_frame
     ; "parsing close frame", `Quick, test_parsing_close_frame
     ; "parsing text frame",  `Quick, test_parsing_text_frame
+    ; "parsing fin bit",  `Quick, test_parsing_fin_bit
     ]
 end
 
