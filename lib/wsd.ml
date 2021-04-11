d96209b67a34ccc115c6d1742d0ab5c3cc043eaa
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2021-04-11 15:50:22 -0700

    OCaml 4.12 fixes, syntax tweaks

--- lib/wsd.ml
+++ lib/wsd.ml
@@ -60,14 +60,14 @@ let send_pong t =
 let flushed t f = Faraday.flush t.faraday f
 
 let close ?code t =
-  let () = match code with
-  | Some code -> 
+  begin match code with
+  | Some code ->
     let mask = mask t in
     let payload = Bytes.create 2 in
     Bytes.set_uint16_be payload 0 (Websocket.Close_code.to_int code);
     Websocket.Frame.serialize_bytes t.faraday ?mask ~is_fin:true ~opcode:`Connection_close ~payload ~off:0 ~len:2;
   | None -> ()
-  in
+  end;
   Faraday.close t.faraday;
   wakeup t
 
--- nix/sources.nix
+++ nix/sources.nix
@@ -1,18 +1,18 @@
-{ ocamlVersion ? "4_10" }:
+{ ocamlVersion ? "4_12" }:
 
 let
   overlays =
     builtins.fetchTarball
-      https://github.com/anmonteiro/nix-overlays/archive/957c4eb.tar.gz;
+      https://github.com/anmonteiro/nix-overlays/archive/1e00a821.tar.gz;
 
 in
 
-  import "${overlays}/sources.nix" {
-    overlays = [
-      (import overlays)
-      (self: super: {
-        ocamlPackages = super.ocaml-ng."ocamlPackages_${ocamlVersion}".overrideScope'
-            (super.callPackage "${overlays}/ocaml" {});
-      })
-    ];
-  }
+import "${overlays}/sources.nix" {
+  overlays = [
+    (import overlays)
+    (self: super: {
+      ocamlPackages = super.ocaml-ng."ocamlPackages_${ocamlVersion}".overrideScope'
+        (super.callPackage "${overlays}/ocaml" { });
+    })
+  ];
+}
