ad4e61eace163611f797dcdf6af801b95d6aee63
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2021-04-11 19:33:00 -0700

    Build examples in CI (#31)

--- nix/ci/test.nix
+++ nix/ci/test.nix
@@ -4,6 +4,28 @@ let
   pkgs = import ../sources.nix { inherit ocamlVersion; };
   inherit (pkgs) lib stdenv fetchTarball ocamlPackages;
 
-  websocketafPkgs = import ./.. { inherit ocamlVersion; };
+  websocketafPkgs = pkgs.recurseIntoAttrs (import ./.. { inherit ocamlVersion; doCheck = true; });
+  websocketafDrvs = lib.filterAttrs (_: value: lib.isDerivation value) websocketafPkgs;
+
 in
-  pkgs.recurseIntoAttrs websocketafPkgs
+
+stdenv.mkDerivation {
+  name = "websocketaf-examples";
+  src = ./../..;
+  dontBuild = true;
+  installPhase = ''
+    touch $out
+  '';
+  buildInputs = (lib.attrValues websocketafDrvs) ++
+    (with ocamlPackages; [
+      ocaml
+      dune
+      findlib
+      httpaf-lwt-unix
+      httpaf-async
+    ]);
+  doCheck = true;
+  checkPhase = ''
+    dune build @examples/all --display=progress
+  '';
+}
