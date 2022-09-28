2d4aa8d29bcb77c3b71d770b7d3080fb5e45b546
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2022-09-28 11:15:22 -0700

    websocketaf-eio: reorg (#45)
    * websocketaf-eio: reorg
    
    * update flake.lock

--- eio/websocketaf_eio.mli
+++ eio/websocketaf_eio.mli
@@ -1,8 +1,29 @@
-module type Server = Websocketaf_eio_intf.Server
+open Websocketaf
 
-module type Client = Websocketaf_eio_intf.Client
+module Server : sig
 
-module Server : Server
-  with type socket = Eio.Net.stream_socket
+  val create_connection_handler
+    :  ?config : Httpaf.Config.t
+    -> websocket_handler : (Eio.Net.Sockaddr.stream -> Wsd.t -> Server_connection.input_handlers)
+    -> error_handler : (Eio.Net.Sockaddr.stream -> Server_connection.error_handler)
+    -> (Eio.Net.Sockaddr.stream -> Eio.Flow.two_way -> unit)
+end
 
-module Client : Client with type socket := Eio.Net.stream_socket
+module Client : sig
+  type t
+
+  (* Perform HTTP/1.1 handshake and upgrade to WS. *)
+  val connect
+    :  ?config : Httpaf.Config.t
+    -> sw             : Eio.Switch.t
+    -> nonce          : string
+    -> host           : string
+    -> port           : int
+    -> resource       : string
+    -> error_handler : (Client_connection.error -> unit)
+    -> websocket_handler : (Wsd.t -> Client_connection.input_handlers)
+    -> Eio.Flow.two_way
+    -> t
+
+  val is_closed : t -> bool
+end
