2b4989347e8ad8cb4003d698cdcaba05461ba1c5
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2024-06-17 22:19:27 -0700

    fix nix flakes

--- flake.nix
+++ flake.nix
@@ -4,8 +4,7 @@
   inputs.nix-filter.url = "github:numtide/nix-filter";
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.nixpkgs.inputs.flake-utils.follows = "flake-utils";
-  # inputs.nixpkgs.url = "github:anmonteiro/nix-overlays";
-  inputs.nixpkgs.url = "/Users/anmonteiro/monorepo/nix-overlays";
+  inputs.nixpkgs.url = "github:anmonteiro/nix-overlays";
 
   outputs = { self, nixpkgs, flake-utils, nix-filter }:
     flake-utils.lib.eachDefaultSystem (system:
